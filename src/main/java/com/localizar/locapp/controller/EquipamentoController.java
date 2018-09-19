cefe4afb83634c782c10d6af19cc1814c75f87bd
EllenCoe <ellenxavier1@hotmail.com>
2018-09-19 20:33:28 -0300

    Persistencia de dados no MySql

--- src/main/java/com/localizar/locapp/controller/EquipamentoController.java
+++ src/main/java/com/localizar/locapp/controller/EquipamentoController.java
@@ -1,15 +1,29 @@
 package com.localizar.locapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.localizar.locapp.models.Equipamento;
+import com.localizar.locapp.repository.EquipamentoRepository;
 
 
 @Controller
 public class EquipamentoController {
+	@Autowired
+	private EquipamentoRepository er;
 	
-	@RequestMapping(value="/cadastrarEquipamento")
+	
+	@RequestMapping(value="/cadastrarEquipamento", method = RequestMethod.GET)
 	public String form(){
 		return "equipamento/formEquipamento";
+	}
+	
+	@RequestMapping(value="/cadastrarEquipamento",method=RequestMethod.POST)
+	public String form(Equipamento equipamento){
+		er.save(equipamento);
+		return "redirect:/cadastrarEquipamento";
 	}
 
 }
---
+++ src/main/java/com/localizar/locapp/repository/EquipamentoRepository.java
@@ -0,0 +1,9 @@
+package com.localizar.locapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.localizar.locapp.models.Equipamento;
+
+public interface EquipamentoRepository  extends CrudRepository<Equipamento, String>{
+
+}
