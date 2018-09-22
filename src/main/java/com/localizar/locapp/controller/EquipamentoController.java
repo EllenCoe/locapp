5550247cb3e2c4b86a65f1e168a11624bdd8e37a
EllenCoe <ellenxavier1@hotmail.com>
2018-09-22 11:46:03 -0300

    Imprimindo Detalhes do equipamento na Tela

--- src/main/java/com/localizar/locapp/controller/EquipamentoController.java
+++ src/main/java/com/localizar/locapp/controller/EquipamentoController.java
@@ -2,6 +2,7 @@ package com.localizar.locapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -36,5 +37,14 @@ public class EquipamentoController {
 		
 		
 	}
-
+	@RequestMapping("/{codigo}")
+	public ModelAndView detalhesEquipamento(@PathVariable("codigo") long codigo){
+		
+		Equipamento equipamento = er.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView ("equipamento/detalhesEquipamento");
+		mv.addObject("equipamento", equipamento);
+		System.out.println("Equipamento" + equipamento);
+		
+		return mv;
+	}
 }
--- src/main/java/com/localizar/locapp/repository/EquipamentoRepository.java
+++ src/main/java/com/localizar/locapp/repository/EquipamentoRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.localizar.locapp.models.Equipamento;
 
 public interface EquipamentoRepository  extends CrudRepository<Equipamento, String>{
+	Equipamento findByCodigo(long codigo);
+	
 
 }
